b92df8056423ec9f6b979a201fdd57136e4323cf
Kazenomi <Artem080605@gmail.com>
2025-12-11 13:36:13 +0300

    feat(music_composer_agent): add initial structure and files for music co [constants.py]

---
+++ music_composer_agent/constants.py
@@ -0,0 +1,4 @@
+"""Constants for the music composer agent."""
+
+VALID_KEYS = ["C major", "C minor", "D major", "D minor", "E major", "E minor", "F major", "F minor", "G major", "G minor", "A major", "A minor", "B major", "B minor"]
+VALID_STYLES = ["classical", "jazz", "rock", "pop"]
