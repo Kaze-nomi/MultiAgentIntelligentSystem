4a89b6dbac739b30e7739899d7365ce1fa74e1b3
Kazenomi <Artem080605@gmail.com>
2025-12-23 08:14:39 +0300

    feat: add meow_meow.cpp and update documentation [meow_meow.cpp]

---
+++ meow_meow.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/**
+ * Simple C++ program that prints "Meow Meow" to the console.
+ */
+int main() {
+    // Print the message
+    std::cout << "Meow Meow" << std::endl;
+
+    // Return success
+    return 0;
+}
