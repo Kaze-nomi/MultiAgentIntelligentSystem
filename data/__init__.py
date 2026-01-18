96849b79dbb82d8e116c8a576798a43e83f34255
Kazenomi <Artem080605@gmail.com>
2026-01-18 18:49:50 +0300

    feat(weather): add weather API to documentation agent [__init__.py]

---
+++ data/__init__.py
@@ -0,0 +1,12 @@
+"""
+Data package initialization.
+
+This package contains data models and structures used throughout the application,
+such as weather data representations.
+"""
+
+from .weather_data import WeatherData
+
+__all__ = [
+    "WeatherData",
+]
